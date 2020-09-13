d9c244ca18489b033bc3afb3c726dcdd011b2387
Joshua Enokson <kilograhm@pm.me>
2020-09-13 19:46:00 +0400

    added spec_only example

---
+++ examples/spec_only.rs
@@ -0,0 +1,29 @@
+fn main() {
+    add_one(0);
+}
+
+fn add_one (x: u64) -> u64 { x + 1 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use laboratory::{describe, it, expect, it_only};
+
+    #[test]
+    fn suite() {
+
+        describe("add_one()").specs(vec![
+
+            it("should return 1", |_| {
+                expect(add_one(0)).to_equal(1)
+            }),
+
+            it_only("should return 2", |_| {
+                expect(add_one(1)).to_equal(2)
+            })
+
+        ]).run();
+
+    }
+}
