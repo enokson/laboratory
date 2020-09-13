9d222bd3bbc93efbbb1fc3953d334325f4fa447b
Joshua Enokson <kilograhm@pm.me>
2020-09-13 22:03:31 +0400

    fixed typos

--- examples/should_panic.rs
+++ examples/should_panic.rs
@@ -2,7 +2,7 @@
 
 fn main() {}
 
-fn panic_at_at_disco(should_panic: bool) {
+fn panic_at_the_disco(should_panic: bool) {
     if should_panic {
         panic!("at the disco");
     }
@@ -12,21 +12,19 @@ fn panic_at_at_disco(should_panic: bool) {
 mod tests {
 
     use laboratory::{describe, it};
-    // use std::panic::{set_hook, take_hook, catch_unwind};
-
     use super::*;
 
     #[test]
     fn test() {
-        describe("panic_at_at_disco()")
+        describe("panic_at_the_disco()")
             .specs(vec![
 
                 it("should panic when passed true", |_| {
-                    should_panic!(panic_at_at_disco, || { panic_at_at_disco(true); })
+                    should_panic!(panic_at_the_disco, || { panic_at_the_disco(true); })
                 }),
 
                 it("should not panic when passed false", |_| {
-                    should_not_panic!(panic_at_at_disco, || { panic_at_at_disco(false); })
+                    should_not_panic!(panic_at_the_disco, || { panic_at_the_disco(false); })
                 })
 
             ]).run();
