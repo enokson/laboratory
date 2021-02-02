386b25a8d8c4d3537e629cc958c4d3f148a28108
Joshua Enokson <kilograhm@pm.me>
2021-02-02 23:57:46 +0500

    exported Suite for public use

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 mod assertion;
 mod suite;
 
-pub use suite::{describe};
+pub use suite::{describe, Suite};
 pub use assertion::{expect, should_panic, should_not_panic};
 
 pub type LabResult = Result<(), String>;
