200c2b6a8bf7494ad64488e43652c806c2eb6cf5
Joshua Enokson <kilograhm@pm.me>
2021-02-03 09:48:02 +0500

    removed old todo and added new todo list

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
 #![allow(dead_code)]
 
-// TODO: fix examples
+/* 
+
+TODO: finish examples/state.rs
+TODO: add examples for new features: retries, slow, skip
+TODO: add example for dynamic tests with if statements and for loops
+TODO: rewrite example comments for updates
+TODO: build docs
+
+
+*/
 
 mod assertion;
 mod suite;
